24822d1bd49cd54dde3233b3c2af52222b2e10b2
RtjShreyD <shreyanshece1041@gmail.com>
2020-05-20 04:47:32 +0530

    resp tested, rec, uploading, still once in a loop, next make dynamic

--- parcello.py
+++ parcello.py
@@ -19,6 +19,8 @@ trans = 0
 PS = False # pin state resp from Arduino
 cams = False # flag var for webrtc video started
 
+kcw = KeyClipWriter(bufSize=32)
+
 conf = Conf("config/config.json")
 up = Uploader(conf)
 
@@ -27,8 +29,69 @@ path = ''
 try:
     arduino = serial.Serial('/dev/ttyUSB0', 9600, timeout=.1)
     time.sleep(1)
+
 except:
     print("Arduino not connected")
+
+
+def ard_snd(msg):
+    try:
+        arduino.write(msg.encode())
+
+    except:
+        print("Arduino not connected")
+
+
+def resp():
+    global PS, trans
+    prev_st = 0
+    curr_st = 0
+
+    while True:
+        data = arduino.readline()
+        if data:
+            temp = data.decode()
+            PS = temp.rstrip()
+            print("PS changed to  " + PS)
+
+            if PS == "True":
+                prev_st = 1
+                curr_st = 1
+
+            if PS == "False" and prev_st == 1:
+                curr_st = 0
+           
+        if prev_st != curr_st:
+            prev_st = 0
+            trans = 1
+            cams = False
+            print("Transaction Complete, Ready for another transaction") #Here we can notify the user about a complete transaction
+
+
+@cam.subscribe
+def onFrame(frame):
+    
+    global path, trans
+        
+    fr = frame
+    fr = imutils.resize(fr, width=600)
+
+    if cams and trans==0:
+        
+        if not kcw.recording:
+            timestamp = datetime.datetime.now()
+            path = "{}/{}.avi".format("vids/", timestamp.strftime("%Y%m%d-%H%M%S"))
+            kcw.start(path, cv2.VideoWriter_fourcc(*"MJPG"), 30)
+        
+    kcw.update(fr)  
+
+    if kcw.recording and trans == 1: ## For when user opens from webrtc page and box get closed by auto
+        print("Rec Stop with transaction Complete" + " " + path)
+        kcw.finish()
+        up.send(path)
+
+    if kcw.recording:
+        print("Rec")
 
 
 class Socketrunthread():
@@ -53,30 +116,28 @@ class Socketrunthread():
             @self.conn.subscribe
             def OnMessage(m):
                 print(m)
-
+                if m == "open":
+                    ard_snd("*")
 
 
     def start_loop(self):
-        
         try:        
             asyncio.set_event_loop(self.loop)
-            # print("Next stmt loop forever")
-            self.loop.run_forever()
-            # print("After run forever")
-
+            self.loop.run_forever()        
         except KeyboardInterrupt:
             pass           
 
+
     def finish(self):
-        # self.cam.close()
         self.conn.close()
         self.loop.stop()
         self.stop_event = False
         self.thread.join()
         print("Thread stopped")
 
-    async def connect(self):
 
+    async def connect(self):
+        global cams
         ws = Websocket("http://localhost:8080/xyz")
         remoteDescription = await ws.get()
         print("The remote description is of type: " + str(type(remoteDescription)) + "\n" )
@@ -86,18 +147,31 @@ class Socketrunthread():
         print("The robot description is of type: " + str(type(robotDescription)) + "\n" )
         ws.put_nowait(robotDescription)
         print("Started WebRTC")
+        cams = True
         await ws.close()
-
 
 
 try:
     t = Socketrunthread(cam, False)
     t.start()
-    time.sleep(20)
-    t.finish()
-    time.sleep(10)
-    t = Socketrunthread(cam, False)
-    t.start()
+
+    t2 = Thread(target=resp)
+    t2.daemon = True
+    t2.start()
+
+    # if trans == 1:
+    #     t.finish()
+    #     trans = 0
+    #     time.sleep(10)
+    #     t = Socketrunthread(cam, False)
+    #     t.start()
+
+
+    # time.sleep(20)
+    # t.finish()
+    # time.sleep(10)
+    # t = Socketrunthread(cam, False)
+    # t.start()
 
 
 except KeyboardInterrupt:
